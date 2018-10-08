f56f415910a77123d52c10bd96ac198468145bc0
Joop Kiefte <ikojba@gmail.com>
2018-10-09 01:32:34 +0200

    Create command line tool

--- cmd/check/main.go
+++ cmd/check/main.go
@@ -1,6 +1,17 @@
 package main
 
 import "github.com/lapingvino/ueacode"
+import "os"
 
 func main() {
+	if len(os.Args) < 2 {
+		os.Exit(-1)
+		println("mankas kodo")
+	}
+	if ueacode.Check(os.Args[1]) {
+		os.Exit(0)
+	}
+	code := ueacode.Calculate(os.Args[1])
+	println(string(ueacode.Letter(code, false)))
+	os.Exit(code)
 }
